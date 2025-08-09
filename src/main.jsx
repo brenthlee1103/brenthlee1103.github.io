1ab677948e5fc23730ed433ad361a5caf2a0dc11
Brent Lee <brenthlee1103@gmail.com>
2025-08-09 11:46:07 -0700

    removed # from the url

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 
 import App from "./App.jsx";
 
+const redirectParam = new URLSearchParams(window.location.search).get(
+  "redirect"
+);
+if (redirectParam) {
+  window.history.replaceState({}, "", redirectParam);
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
