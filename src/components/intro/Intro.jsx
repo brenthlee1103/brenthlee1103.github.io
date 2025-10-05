6608f13c5682b25ad405de979a21cfc15c573bd4
Brent Lee <brenthlee1103@gmail.com>
2025-10-05 11:37:32 -0700

    complete intro section and responsiveness

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,19 @@
 import "./intro.scss";
+import gifPfp from "@/assets/newpfp.gif";
 
 function Intro() {
   return (
     <section className="intro-section">
-      <h1>intro</h1>
+      <div className="container">
+        <img className="hero-pfp" src={gifPfp} />
+        <div className="intro-text">
+          <span className="intro-title">Hi, I'm Brent Lee.</span>
+          <span className="intro-body">
+            I'm a full-stack software engineer with a focus in front-end, open
+            to opportunities in Bay Area, CA or Orange County, CA.
+          </span>
+        </div>
+      </div>
     </section>
   );
 }
